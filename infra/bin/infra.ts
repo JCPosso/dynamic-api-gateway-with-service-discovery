d02accf56b819c76315ee91d2fdbb2670a8364ef
JCPosso <juancamiloposso01@gmail.com>
2025-11-30 22:50:45 -0500

    feat: reorganize stack initialization in infra.ts and improve code readability

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,15 +2,31 @@ import * as cdk from "aws-cdk-lib";
 import { DynamoDbStack } from "../lib/dynamodb-stack";
 import { LambdaRouterStack } from "../lib/lambda-router-stack";
 import { ApiGatewayStack } from "../lib/api-gateway-stack";
+import { EcsClusterStack } from "@infra/lib/ecs-cluster-stack";
+import { EcsServiceUsersStack } from "@infra/lib/ecs-service-users-stack";
+import { LambdaSyncStack } from "@infra/lib/lambda-sync-stack";
 
 const app = new cdk.App();
 
-const dynamoStack = new DynamoDbStack(app, "ServiceRegistryStack");
+const dynamo = new DynamoDbStack(app, "ServiceRegistryStack");
 
-const routerStack = new LambdaRouterStack(app, "LambdaRouterStack", {
-  serviceRegistryTable: dynamoStack.serviceRegistry,
+const ecsCluster = new EcsClusterStack(app, "EcsClusterStack");
+
+const usersService = new EcsServiceUsersStack(app, "UsersServiceStack", {
+  cluster: ecsCluster.cluster,
+  serviceName: "users",
+});
+
+const router = new LambdaRouterStack(app, "LambdaRouterStack", {
+  serviceRegistryTable: dynamo.serviceRegistry,
 });
 
 new ApiGatewayStack(app, "ApiGatewayStack", {
-  routerLambda: routerStack.routerLambda,
+  routerLambda: router.routerLambda,
+});
+
+new LambdaSyncStack(app, "LambdaSyncStack", {
+  serviceRegistryTable: dynamo.serviceRegistry,
+  clusterName: ecsCluster.cluster.clusterName,
+  ecsServiceName: usersService.service.serviceName,
 });
