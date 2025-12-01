9261ab3f09affa8dbd903c86606f0d3739b9f991
JCPosso <juancamiloposso01@gmail.com>
2025-11-30 20:46:20 -0500

    feat(dynamodb-stack): refactor DynamoDB stack to define service table with partition key and billing mode

--- infra/lib/dynamodb-stack.ts
+++ infra/lib/dynamodb-stack.ts
@@ -1,9 +1,16 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
-export class DynamoDbStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class DynamoDBStack extends Stack {
+  public readonly serviceTable: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    // TODO: crear tabla DynamoDB
+
+    this.serviceTable = new dynamodb.Table(this, "ServiceRegistry", {
+      partitionKey: { name: "serviceName", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
   }
 }
