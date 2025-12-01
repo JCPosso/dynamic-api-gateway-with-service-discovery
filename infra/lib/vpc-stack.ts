36865122aefd2e47617961b6f9584e451532bdac
JCPosso <juancamiloposso01@gmail.com>
2025-11-30 20:46:51 -0500

    feat(vpc-stack): refactor VPC stack to use ec2.Vpc and improve structure

--- infra/lib/vpc-stack.ts
+++ infra/lib/vpc-stack.ts
@@ -1,9 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-export class VpcStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class VpcStack extends Stack {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    // TODO: crear VPC
+
+    this.vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "public"
+        }
+      ]
+    });
   }
 }
